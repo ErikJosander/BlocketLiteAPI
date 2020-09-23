eb24ca90a3d0548ed882b7b688bf35883434209a
Erik Josander <josander.erik@gmail.com>
2020-09-23 15:33:51 +0200

    addd more comments to the interfaces

--- BlocketLiteEFCoreDB/Services/ICommentRepository.cs
+++ BlocketLiteEFCoreDB/Services/ICommentRepository.cs
@@ -1,10 +1,23 @@
 ﻿using BlocketLiteEFCoreDB.Entities;
 using System.Collections.Generic;
+using BlocketLiteEFCoreDB.Repositories;
 
 namespace BlocketLiteEFCoreDB.Services
 {
+    /// <summary>
+    /// Interface that defins contracts for the <see cref="ICommentRepository"/>
+    /// </summary>
     public interface ICommentRepository : IRepository<Comment>
     {
+        /// <summary>
+        /// If <see cref="DbContexts.BlocketLiteContext.Advertisements"/><see cref="Advertisement.Id"/> is equal to <paramref name="realEstateId"/>"/>
+        /// <br></br> returns <see cref="ICollection{T}"/> of <see cref="Comment"/>.
+        /// <br></br> Optional <paramref name="skip"/> and <paramref name="take"/> parameters.
+        /// </summary>
+        /// <param name="realEstateId"></param>
+        /// <param name="skip"></param>
+        /// <param name="take"></param>
+        /// <returns>An <see cref="ICollection{T}"/> of <see cref="Comment"/></returns>
         ICollection<Comment> GetAllFromRealEstate(int realEstateId, int skip, int take);
         ICollection<Comment> GetAllUser(string userName, int skip, int take);
         int? GetUserIdFromUserName(string userName);
