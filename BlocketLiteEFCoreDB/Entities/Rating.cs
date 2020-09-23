b8ff178141e8bda77129312e326e56835275e137
Erik Josander <josander.erik@gmail.com>
2020-09-23 14:31:42 +0200

    finnished the comments for the entities

--- BlocketLiteEFCoreDB/Entities/Rating.cs
+++ BlocketLiteEFCoreDB/Entities/Rating.cs
@@ -3,16 +3,19 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BlocketLiteEFCoreDB.Entities
 {
+    /// <summary>
+    /// Public <see cref="Entities"/> that is stored in the DB.
+    /// </summary>
     public class Rating
     {
         /// <summary>
-        /// Unique id-number 
+        /// Unique identifier
         /// </summary>
         [Key]
         public int Id { get; set; }
 
         /// <summary>
-        /// A value rating from 1-5 a user can place on another user
+        /// A value (<see cref="int"/>) rating from 1-5 an <see cref="User"/> can place on another <see cref="User"/>.
         /// </summary>
         [Range(1, 5)]
         [Required]
@@ -20,7 +23,7 @@ namespace BlocketLiteEFCoreDB.Entities
 
 
         /// <summary>
-        /// ForeignKey to the User table
+        /// <see cref="ForeignKeyAttribute"/> to the rated <see cref="User"/>.
         /// </summary>
         [ForeignKey("RatedUserId")]
         public virtual User RatedUser { get; set; }
@@ -28,7 +31,7 @@ namespace BlocketLiteEFCoreDB.Entities
 
 
         /// <summary>
-        /// ForeignKey to the User table
+        /// <see cref="ForeignKeyAttribute"/> to the rating <see cref="User"/>.
         /// </summary>      
         [ForeignKey("RatingUserId")]
         public virtual User RatingUser { get; set; }
