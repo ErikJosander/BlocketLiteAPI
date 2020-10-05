f107d3b0c9dec9c62583fbfab8e96251afe7c6b7
Erik Josander <josander.erik@gmail.com>
2020-10-05 10:15:26 +0200

    all comments added

--- BlocketLiteEFCoreDB/Repositories/Repository.cs
+++ BlocketLiteEFCoreDB/Repositories/Repository.cs
@@ -7,50 +7,87 @@ using System.Linq.Expressions;
 
 namespace BlocketLiteEFCoreDB.Repositories
 {
+    /// <summary>
+    /// A Generic repository that implements <see cref="IRepository{TEntity}"/>
+    /// </summary>
+    /// <typeparam name="TEntity"></typeparam>
     public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
     {
         private readonly DbContext _contex;
 
+        // Constructor
         public Repository(DbContext context)
         {
             _contex = context ?? throw new ArgumentNullException(nameof(context));
         }
 
+        /// <summary>
+        /// Gets the <see cref="TEntity"/> when a correct <paramref name="id"/> is supplied. 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>A <typeparamref name="TEntity"/>.</returns>
         public TEntity Get(int id)
         {
             return _contex.Set<TEntity>().Find(id);
         }
 
+        /// <summary>
+        /// Gets a <see cref="IEnumerable{T}"/> of the <see cref="TEntity"/>
+        /// </summary>
+        /// <returns><see cref="IEnumerable{T}"/></returns>
         public IEnumerable<TEntity> GetAll()
         {
             return _contex.Set<TEntity>().ToList();
         }
 
+        // Do we need this?
         public IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> expression)
         {
             return _contex.Set<TEntity>().Where(expression);
         }
 
+        // <summary>
+        /// Adds a <see cref="TEntity"/> to the DB.
+        /// </summary>
+        /// <param name="entity"></param>
         public void Add(TEntity entity)
         {
             _contex.Set<TEntity>().Add(entity);
         }
 
+
+        /// <summary>
+        /// Adds a <see cref="IEnumerable{T}"/> of <see cref="TEntity"/> to the DB.
+        /// </summary>
+        /// <param name="entities"></param>
         public void AddRage(IEnumerable<TEntity> entities)
         {
             _contex.Set<IEnumerable<TEntity>>().AddRange(entities);
         }
 
+        /// <summary>
+        /// Removes a <see cref="TEntity"/> from the DB.
+        /// </summary>
+        /// <param name="entity"></param>
         public void Remove(TEntity entity)
         {
             _contex.Set<TEntity>().Remove(entity);
         }
 
+
+        /// <summary>
+        /// Removes an <see cref="IEnumerable{T}"/> of <see cref="TEntity"/> from the DB.
+        /// </summary>
+        /// <param name="entities"></param>
         public void RemoveRange(IEnumerable<TEntity> entities)
         {
             _contex.Set<IEnumerable<TEntity>>().RemoveRange(entities);
         }
 
+
+        /// <summary>
+        /// Saves changes made in the <see cref="DbContexts"/> to the DB.
+        /// </summary>
         public void Save()
         {
             _contex.SaveChanges();
--- BlocketLiteEFCoreDB/Repositories/UserRepository.cs
+++ BlocketLiteEFCoreDB/Repositories/UserRepository.cs
@@ -7,21 +7,36 @@ using System.Linq;
 
 namespace BlocketLiteEFCoreDB.Repositories
 {
+    /// <summary>
+    /// Repository that implements <see cref="IUserRepository"/>
+    /// </summary>
     public class UserRepository : Repository<User>, IUserRepository
     {
         private readonly BlocketLiteContext _context;
 
+        // Constructor
         public UserRepository(BlocketLiteContext context)
             : base(context)
         {
             _context = context ?? throw new ArgumentNullException(nameof(context));
         }
 
+        /// <summary>
+        /// Return all the <see cref="IEnumerable{T}"/> of <see cref="Advertisement"/> in the <see cref="DbContexts.BlocketLiteContext.Advertisements"/>
+        /// <br></br> where <see cref="Advertisement.UserId"/> is equal to <paramref name="userId"/>.
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns><see cref="IEnumerable{T}"/> of <see cref="Advertisement"/></returns>
         public IEnumerable<Advertisement> GetAdvertisements(int userId)
         {
             return _context.Advertisements.Where(ui => ui.UserId == userId);
         }
 
+        /// <summary>
+        /// Returns <see cref="IEnumerable{T}"/> of <see cref="User"/> in the <see cref="DbContexts.BlocketLiteContext.Users"/> table in the DB.
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <returns><see cref="IEnumerable{T}"/> of <see cref="User"/></returns>
         public IEnumerable<User> GetAll(string userName)
         {
             if (string.IsNullOrWhiteSpace(userName))
@@ -40,6 +55,12 @@ namespace BlocketLiteEFCoreDB.Repositories
             return collection.ToList();
         }
 
+        /// <summary>
+        /// If <see cref="DbContexts.BlocketLiteContext.Users"/> contains a <see cref="User"/> with <see cref="User.UserName"/> that is equal to <paramref name="userName"/>.
+        /// <br></br> return it.
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <returns><see cref="User"/></returns>
         public User GetFromUserName(string userName)
         {
             return _context.Users.Where(u => u.UserName == userName).FirstOrDefault();
