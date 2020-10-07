cbae78949f4b15dac96aee88c5e069238380d51e
Erik Josander <josander.erik@gmail.com>
2020-10-07 14:32:36 +0200

    code added

--- BlocketLiteAPI/Models/User/UserDto.cs
+++ BlocketLiteAPI/Models/User/UserDto.cs
@@ -8,6 +8,11 @@ namespace BlocketLiteAPI.Models
     public class UserDto
     {
         /// <summary>
+        /// The unique Id of a a user (GUID)
+        /// </summary>
+        public string UserId { get; set; }
+
+        /// <summary>
         /// The username of an <see cref="User"/>
         /// </summary>
         public string UserName { get; set; }
